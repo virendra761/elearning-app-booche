4b4241365ebf0be3965952f0706562446c4879cb
jainusri49 <janendrasrivastava49@gmail.com>
2023-12-29 15:29:07 +0530

    initial commit

---
+++ src/main/java/in/ashokit/security/PwdSecurityService.java
@@ -0,0 +1,13 @@
+package in.ashokit.security;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class PwdSecurityService {
+	
+	public String encode(String text) {		
+		Encoder encoder = Base64.getEncoder();
+		String encodeToString = encoder.encodeToString(text.getBytes());
+		return encodeToString;
+	}
+}
